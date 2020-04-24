9bf55d45a915335f2656cc24dd4407158738c947
Patrick Hadson <pkhadson@gmail.com>
2020-04-24 12:18:42 -0300

    a

---
+++ index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+//var server = express.createServer();
+// express.createServer()  is deprecated. 
+var server = express(); // better instead
+
+  server.use(express.static(__dirname + '/public'));
+
+server.listen(80);
